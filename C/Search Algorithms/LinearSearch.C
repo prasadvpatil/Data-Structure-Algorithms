0c8b628062de59ece8ff65a764c6f4b680283a68
pbisen <65162182+pbisen@users.noreply.github.com>
2020-10-01 23:26:01 +0530

    Update LinearSearch.C

--- C/Search Algorithms/LinearSearch.C
+++ C/Search Algorithms/LinearSearch.C
@@ -3,25 +3,20 @@
 // is present then return its location, otherwise 
 // return -1 
   
-#include <iostream> 
-using namespace std; 
-  
-int search(int arr[], int n, int x) 
+#include <stdio.h> 
+
+int main() 
 { 
-    int i; 
-    for (i = 0; i < n; i++) 
-        if (arr[i] == x) 
-            return i; 
-    return -1; 
-} 
-  
-int main(void) 
-{ 
-    int arr[] = { 2, 3, 4, 10, 40 }; 
+    int arr[5] = {2,3,4,10,40}; 
     int x = 10; 
-    int n = sizeof(arr) / sizeof(arr[0]); 
-    int result = search(arr, n, x); 
-   (result == -1)? cout<<"Element is not present in array" 
-                 : cout<<"Element is present at index " <<result; 
-   return 0; 
+    for(int i = 0; i < 5; i++)
+	{
+	    if(arr[i] == x)
+	    {
+	        printf("Index: %d", i);
+	        return 0;
+	    }
+	}
+	printf("Not Found");
+	return -1;
 } 
