fdb83eed69f22ec9640055467e392eda96938091
Prasad V Patil <69467300+prasadvpatil@users.noreply.github.com>
2020-10-01 18:02:26 +0530

    Update Quick_Sort.cpp

--- C++/Quick_Sort.cpp
+++ C++/Quick_Sort.cpp
@@ -5,9 +5,6 @@ using namespace std;      //For cin and cout
     Quicksort is a divide-and-conquer algorithm. It works by selecting a 'pivot' element from the array
     and partitioning the other elements into two sub-arrays, according to whether they are less than or greater than the pivot. 
     The sub-arrays are then sorted recursively. This can be done in-place, requiring small additional amounts of memory to perform the sorting.
-
-    @author Aditya Saxena
-    @since 27-6-2020
 */
 
 //Implement partitionPivot function
